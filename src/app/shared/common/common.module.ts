4378490b2a450140150236d2c90c0b71c015d4b7
Vovan_Super <ovsyukov@yandex.com>
2020-03-24 22:04:37 +0300

    include styles to bundle

--- src/app/shared/common/common.module.ts
+++ src/app/shared/common/common.module.ts
@@ -3,14 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { PopoverMenuComponent } from './popover-menu/popover-menu.component';
-import {
-  HeaderComponent,
-  ContentContainerComponent,
-  AboutModalComponent,
-  DataPrivacyModalComponent,
-  ImprintModalComponent
-} from './cmps';
-
+import { HeaderComponent, ContentContainerComponent, AboutModalComponent, DataPrivacyModalComponent, ImprintModalComponent } from './cmps';
 
 @NgModule({
   imports: [
