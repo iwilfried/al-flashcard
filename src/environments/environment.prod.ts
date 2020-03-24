ddacf06a23ef256809b4f7c3625c51408d1f5adb
Vovan_Super <ovsyukov@yandex.com>
2020-03-24 22:12:28 +0300

    add firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAekMXsEzvkOGAflkpANAk9bFte7TlsJpA",
+    authDomain: "flashcard-a9837.firebaseapp.com",
+    databaseURL: "https://flashcard-a9837.firebaseio.com",
+    projectId: "flashcard-a9837",
+    storageBucket: "flashcard-a9837.appspot.com",
+    messagingSenderId: "825432761170",
+    appId: "1:825432761170:web:895409c2b50c15f650508b"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAekMXsEzvkOGAflkpANAk9bFte7TlsJpA",
+    authDomain: "flashcard-a9837.firebaseapp.com",
+    databaseURL: "https://flashcard-a9837.firebaseio.com",
+    projectId: "flashcard-a9837",
+    storageBucket: "flashcard-a9837.appspot.com",
+    messagingSenderId: "825432761170",
+    appId: "1:825432761170:web:895409c2b50c15f650508b"
+  }
 };
 
 /*
